292460dea8275f4ff1d04cb70d95eae00e8d4b1d
Oliver <olivermihaly97@gmail.com>
2024-08-13 17:08:50 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import NasConnector from "./NasConnector";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>NAS Connector</h1>
+      <NasConnector />
+    </div>
+  );
+};
+
+export default App;
